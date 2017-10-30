1e4e58809b042a81f56b6f0dd6e71f5331f086a8
TADI SANTH KUMAR <santhtadi@gmail.com>
2017-10-31 02:28:14 +0530

    Update sudoku V1.py

--- sudoku V1.py
+++ sudoku V1.py
@@ -5,7 +5,7 @@ Created on Thu Oct 26 17:33:39 2017
 @author: santh
 """
 
-sudoku=["enter all elements row wose from left to right; 0 indicates a gap"]
+sudoku=["enter all elements row wise from left to right; 0 indicates a gap"]
 row_indices=[[0,1,2,3,4,5,6,7,8],
              [9,10,11,12,13,14,15,16,17],
              [18,19,20,21,22,23,24,25,26],
